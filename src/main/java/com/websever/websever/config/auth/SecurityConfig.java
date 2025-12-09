6034d9d868a7888b86120c34ac5da2331c35405b
chldPDms <115327796+chldPDms@users.noreply.github.com>
2025-12-09 10:08:12 +0900

    cors 에러

--- src/main/java/com/websever/websever/config/auth/SecurityConfig.java
+++ src/main/java/com/websever/websever/config/auth/SecurityConfig.java
@@ -30,12 +30,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOriginPatterns(List.of(
-                "http://localhost:8080",
-                "https://webserver-backend.onrender.com"
-        ));
-
-
+        configuration.setAllowedOriginPatterns(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
